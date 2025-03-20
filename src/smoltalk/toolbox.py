67e179869a527c914a998a2754888666aea96cb8
Adam Bradley <hisself@adambradley.net>
2025-03-20 13:20:35 -0400

    Handle tool errors better.

--- src/smoltalk/toolbox.py
+++ src/smoltalk/toolbox.py
@@ -10,12 +10,14 @@ logger = logging.getLogger(__name__)
 
 class Toolbox():
     def __init__(
-        self, tools: Union[Type[object], object], root_url: str, model: str, api_key: str='no-key-needed', system_prompt: str = None
+        self, tools: Union[Type[object], object], root_url: str, model: str, api_key: str='no-key-needed', 
+        system_prompt: str = None, fail_on_tool_error: bool = False
     ):
         self.tools = tools
         self.root_url = root_url
         self.model = model
         self.api_key = api_key
+        self.fail_on_tool_error = fail_on_tool_error
 
         here = os.path.dirname(__file__)
 
@@ -24,50 +26,61 @@ class Toolbox():
 
         self.system_prompt = system_prompt
 
-    async def get_response(self, messages, auto_tool_call=True, fail_on_tool_error=True):
+        self.tool_signatures = self._generate_tool_signatures()
+
+    async def get_response(self, messages, auto_tool_call=True, fail_on_tool_error=None):
+        if fail_on_tool_error is None:
+            fail_on_tool_error = self.fail_on_tool_error
         if self.system_prompt:        
             messages.insert(0, {"role": "system", "content": self.system_prompt})
         logger.debug("Getting a response from the model at %s" % (self.root_url,))
-        print(self.root_url, f"{self.root_url}chat/completions",)
         request_body = {
             "model": self.model,
             "messages": messages,
-            "tools": self._generate_tool_signatures(),
+            "tools": self.tool_signatures,
             "tool_choice": "auto",
             "n": 1,
         }
-        print('request_body', request_body)
+        logger.debug('request_body', json.dumps(request_body))
         async with httpx.AsyncClient() as client:
             response = await client.post(
                 f"{self.root_url}chat/completions",
                 headers={"Authorization": f"Bearer {self.api_key}"},
                 json=request_body,
                 follow_redirects=True,
+                timeout=15,
             )
-        print(response)
         response = response.json()
-        logger.debug("Response from model: %s" % (str(response),))
-        print("Response from model: %s" % (str(response),))
-
-        if auto_tool_call:
-            # TODO: this should be async and call the tools in parallel.
-            for tool_call in response['choices'][0]['message'].get('tool_calls', []):
-                print(type(tool_call))
-                response = await self._call_tool(tool_call)
-                logger.debug("TOOL RESPONSE:", response)
-                messages.append({"role": "assistant", "content": response})
-                response = await self.get_response(messages)
-                response = response.json()
-
         print(response)
+        logger.debug("Response from model: %s" % (str(response),))
+        messages.append(response['choices'][0]['message'])
+        
+        if auto_tool_call and response['choices'][0]['message'].get('tool_calls', []):
+            # TODO: this should be async and call the tools in parallel.
+            for tool_call in response['choices'][0]['message'].get('tool_calls'):
+                try:
+                    response = await self._call_tool(tool_call)
+                    logger.debug("tool response:", response)
+                    if fail_on_tool_error and response.get('error'):
+                        logger.warning("Tool call failed with error: %s" % (response['error'],))
+                        return response
+                #TODO: provide a more specific exception for tools to throw.
+                except Exception as e:
+                    logger.warning("Tool call failed with exception: %s" % (str(e),))
+                    response = {"error": "Tool call failed with exception: %s" % (str(e),)}
+                    if fail_on_tool_error:
+                        return response
+                messages.append({"role": "tool", "content": json.dumps(response), "tool_call_id": tool_call['id']})
+                response = await self.get_response(messages)
+
         return response
 
-    async def _call_tool(cls, tool_call):
+    async def _call_tool(self, tool_call):
         logger.debug("_call_tool: %s" % (tool_call,))
-        tool_name = tool_call.function.name
-        tool_args = json.loads(tool_call.function.arguments)
+        tool_name = tool_call['function']['name']
+        tool_args = json.loads(tool_call['function']['arguments'])
         logger.debug("Calling tool '%s' with parameters '%s'" % (tool_name, tool_args))
-        tool = getattr(cls.Tools, tool_name)
+        tool = getattr(self.tools, tool_name)
         if inspect.iscoroutinefunction(tool):
             return await tool(**tool_args)
         else:
@@ -167,16 +180,19 @@ def function_to_dict(input_function):  # noqa: C901
 
     # Create the dictionary
     result = {
-        "name": name,
-        "description": description,
-        "parameters": {
-            "type": "object",
-            "properties": parameters,
-        },
+        "type": "function", 
+        "function": {
+            "name": name,
+            "description": description,
+            "parameters": {
+                "type": "object",
+                "properties": parameters,
+            },
+        }
     }
 
     # Add "required" key if there are required parameters
     if required_params:
-        result["parameters"]["required"] = required_params
+        result["function"]["parameters"]["required"] = required_params
 
     return result
